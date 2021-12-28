fc2e6a93c89895a4676bd7e28a905acdc0a4b296
orange006 <alexeyefimov15@gmail.com>
2021-12-28 19:44:41 +0200

    Created header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  return (
+    <div>
+      <div className='header'>
+        <h1 className='brand'>
+          <i class="fab fa-bitcoin"></i> CryptoTracker
+        </h1>
 
+        <form>
+          <input className='inputField' placeholder='Search a coin' type='text' />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
